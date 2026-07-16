735113060ef916b3ded88f4cf7f7fd73e8b193e5
AlexVerdugo <alex.verdugo.-@hotmail.com>
2026-07-16 18:29:16 -0400

    fix: resolve drawer export path in design system

--- frontend/src/design-system/layouts/index.js
+++ frontend/src/design-system/layouts/index.js
@@ -1,3 +1,3 @@
 export { default as DashboardGrid } from "./DashboardGrid";
 export { default as Section } from "./Section";
-export { default as Drawer } from "./Drawer";
+export { default as Drawer } from "./drawer";
